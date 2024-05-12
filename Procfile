3f63b7568abbeb542128924a45c10031cc082b3c
unnath0 <timeandwind01@gmail.com>
2024-05-12 11:43:19 +0530

    deleted:    Procfile 	modified:   digitalRecipeCookbook/__pycache__/settings.cpython-312.pyc 	modified:   digitalRecipeCookbook/settings.py 	new file:   railway.json 	modified:   requirements.txt 	deleted:    runtime.txt

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn digitalRecipeCookbook.wsgi --log-file -
--- digitalRecipeCookbook/settings.py
+++ digitalRecipeCookbook/settings.py
@@ -109,10 +109,11 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'mainFiles/staticfiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "mainFiles/static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    BASE_DIR / "mainFiles/static"
-]
